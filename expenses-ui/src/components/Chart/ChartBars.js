67d906d59ae0427c23181d58d619225362367bc0
nurlankuluev <nurlan.devprojects@gmail.com>
2022-12-24 20:15:23 +0600

    all files

---
+++ expenses-ui/src/components/Chart/ChartBars.js
@@ -0,0 +1,23 @@
+import "./ChartBars.css";
+import React from "react";
+
+const ChartBars = (props) => {
+  let BarFillHeight = "0%";
+
+  if (props.maxValue > 0) {
+    BarFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: BarFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBars;
--- expenses-ui/src/components/UserInput/NewExpense.js
+++ expenses-ui/src/components/UserInput/NewExpense.js
@@ -1,17 +1,37 @@
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false); //! state for editing 'Add New Expense' button
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
-    props.onAddExpense(expenseData)
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {!isEditing && ( //! if isEditing is false
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
